7d38e7fa92a259f36f16a9d9bd1787cb4655ba8c
Gregory Drogaline <greg.drogaline@gmail.com>
2019-01-31 11:40:36 -0600

    Upgrade to Spring boot 2.1.2 and Swagger 2.9.2

--- spring-boot-2-rest-service-with-swagger/src/main/java/com/in28minutes/springboot/rest/example/swagger/SwaggerConfig.java
+++ spring-boot-2-rest-service-with-swagger/src/main/java/com/in28minutes/springboot/rest/example/swagger/SwaggerConfig.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -17,23 +18,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-  public static final Contact DEFAULT_CONTACT = new Contact(
-      "Ranga Karanam", "http://www.in28minutes.com", "in28minutes@gmail.com");
-  
-  public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
-      "Awesome API Title", "Awesome API Description", "1.0",
-      "urn:tos", DEFAULT_CONTACT, 
-      "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	public static final Contact DEFAULT_CONTACT = new Contact("Ranga Karanam", "http://www.in28minutes.com",
+			"in28minutes@gmail.com");
 
-  private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = 
-      new HashSet<String>(Arrays.asList("application/json",
-          "application/xml"));
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
+			Arrays.asList("application/json", "application/xml"));
 
-  @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .apiInfo(DEFAULT_API_INFO)
-        .produces(DEFAULT_PRODUCES_AND_CONSUMES)
-        .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
-  }
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfoBuilder().title("Awesome API Title")
+			.description("Awesome API Description").contact(DEFAULT_CONTACT).license("Apache 2.0")
+			.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0").version("1.0").build();
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO).produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+	}
+
+
 }
